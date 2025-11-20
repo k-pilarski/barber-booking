efba16e41fec1cff009763d2caa8052c01a37b8a
k-pilarski <karolpilarski04@gmail.com>
2025-11-20 16:22:00 +0100

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/example/barberbooking/BarberbookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.barberbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/barberbooking/BarberbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barberbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
